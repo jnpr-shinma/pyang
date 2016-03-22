32ddc85f9d6c1151286711580d0c29e7a0fb69f3
Shin Ma <shinma@juniper.net>
2016-03-22 15:20:08 +0800

    d

--- pyang/plugins/gesm.py
+++ pyang/plugins/gesm.py
@@ -48,16 +48,10 @@ def mapping(ctx, modules):
             mapping={}
             module_node=mapping[module.arg]={}
             mappings_node=module_node['mappings']={}
-            for vertex in module.i_children:
-                if vertex.keyword in ('list','container'):
-                    document_type_name = vertex.arg.replace('-', '_')
-                    if hasattr(vertex, 'i_children'):
-                        children = vertex.i_children
-                        node=mappings_node[document_type_name]={}
-                        node_prop=node['properties']={}
-                        for child in children:
-                            map_node(child, module,node_prop,document_type_name)
-                    # map_node(child,module,mappings_node,child.arg)
+            for child in module.i_children:
+                if child.keyword=='list':
+                    document_type_name=child.arg.replace('-','_')
+                    map_node(child,module,mappings_node,document_type_name)
             file='%s%s%s.mapping.json'%(ctx.opts.directory,os.sep,module.arg)
             try:
                 os.makedirs(ctx.opts.directory, 0o777)
@@ -77,60 +71,61 @@ def mapping(ctx, modules):
 
 def map_node(s, module,parent_node,document_type_name):
     node_name = s.arg
+    _node_name = node_name.replace('-', '_')
     if s.keyword in ('leaf-list','leaf'):
         yang_type=get_build_in_type(s).arg
         es_type=get_es_type(yang_type)
         if es_type == 'string':
-            parent_node[node_name]={'type':'string','index':'analyzed','fields':{'_raw':{'type':'string','index':'not_analyzed'},'_%s_suggest'%document_type_name:{'type':'completion'}}}
+            parent_node[_node_name]={'type':'string','index':'analyzed','fields':{'_raw':{'type':'string','index':'not_analyzed'},'_%s_suggest'%document_type_name:{'type':'completion'}}}
         elif es_type=='double':
-            parent_node[node_name]={'type':'double','index':'analyzed','fields':{'_raw':{'type':'double','index':'not_analyzed'}}}
+            parent_node[_node_name]={'type':'double','index':'analyzed','fields':{'_raw':{'type':'double','index':'not_analyzed'}}}
         elif es_type=='boolean':
-            parent_node[node_name]={'type':'boolean'}
+            parent_node[_node_name]={'type':'boolean'}
         elif es_type=='binary':
-            parent_node[node_name]={'type':'binary'}
+            parent_node[_node_name]={'type':'binary'}
     elif s.keyword in ('choice','case')  and hasattr(s, 'i_children'):
             children = s.i_children
             for child in children:
                 map_node(child, module,parent_node,document_type_name)
     elif hasattr(s, 'i_children'):
             children = s.i_children
-            node=parent_node[node_name]={}
+            node=parent_node[_node_name]={}
             node_prop=node['properties']={}
             for child in children:
                 map_node(child, module,node_prop,document_type_name)
 
 
-def get_typename(s):
-    t = s.search_one('type')
-    if t is not None:
-        if t.arg == 'leafref':
-            refnode=t.parent.i_leafref.i_target_node
-            return refnode.arg
-        else:
-            return t.arg
-    else:
-        return ''
-
-
 def get_build_in_type(stmt):
     """Returns the built in type that stmt is derived from"""
-    if stmt.keyword == 'type' and stmt.arg == 'union':
-        return stmt
-    type_stmt = search_one(stmt, 'type')
-    if type_stmt is None:
-        return stmt
-    if type_stmt.arg == 'leafref':
-        ref_node=type_stmt.parent.i_leafref.i_target_node
-        return get_build_in_type(ref_node)
-    try:
-        typedef = type_stmt.i_typedef
-    except AttributeError:
-        return type_stmt
-    else:
-        if typedef is not None:
-            return get_build_in_type(typedef)
+    if stmt.keyword == 'type':
+        if stmt.arg == 'union':
+            subtype=None
+            for substmt in stmt.substmts:
+                if subtype is None:
+                    subtype = get_build_in_type(substmt)
+                else:
+                    new_subtype = get_build_in_type(substmt)
+                    if subtype.arg != new_subtype.arg:
+                        raise RuntimeError("The base type of member type in union type '%s' are not same "%(stmt.parent.arg))
+            return subtype
         else:
-            return type_stmt
+            try:
+                typedef = stmt.i_typedef
+            except AttributeError:
+                return stmt
+            else:
+                if typedef is not None:
+                    return get_build_in_type(typedef)
+                else:
+                    return stmt
+    else:
+        type_stmt = search_one(stmt, 'type')
+        if type_stmt is None:
+            return stmt
+        if type_stmt.arg == 'leafref':
+            ref_node=type_stmt.parent.i_leafref.i_target_node
+            return get_build_in_type(ref_node)
+        return get_build_in_type(type_stmt)
 
 def search_one(stmt, keyword, arg=None):
     """Utility for calling Statement.search_one, including i_children."""
@@ -153,4 +148,6 @@ def get_es_type(yang_type):
         es_type = 'boolean'
     elif yang_type == 'binary':
         es_type = 'binary'
+    else:
+        es_type = 'string'
     return es_type
